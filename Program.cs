f9d9e4ea1282d027781f1b4b177a6a670636feee
Vishal Singh <VishalSingh1597>
2021-05-12 01:31:50 +0530

    [Vishal]Added:Welcome Code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---------Welcome To Binary Search Tree--------");
+            Console.WriteLine();
+            BST<int> bST = new BST<int>(56);
+            bST.Insert(30);
+            bST.Insert(70);
+            bST.Display();
+            //    56
+            // 30         70
+
+            Console.Read();
+        }
+    }
+}
